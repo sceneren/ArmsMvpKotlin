c6abdf01a1d97e312936a914c86a527bd00ee7e5
don <13594622565@163.com>
2019-04-22 23:36:06 +0800

    首次提交

---
+++ app/src/main/java/com/daomingedu/art/mvp/model/api/service/UserService.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017 JessYan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.daomingedu.art.mvp.model.api.service
+
+import com.daomingedu.art.mvp.model.entity.BaseJson
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+/**
+ * ================================================
+ * 展示 [Retrofit.create] 中需要传入的 ApiService 的使用方式
+ * 存放关于用户的一些 API
+ *
+ *
+ * Created by JessYan on 08/05/2016 12:05
+ * [Contact me](mailto:jess.yan.effort@gmail.com)
+ * [Follow me](https://github.com/JessYanCoding)
+ * ================================================
+ */
+interface UserService {
+    /*String HEADER_API_VERSION = "Accept: application/vnd.github.v3+json";
+
+    @Headers({HEADER_API_VERSION})
+    @GET("/users")
+    Observable<List<Object>> getUsers(@Query("since") int lastIdQueried, @Query("per_page") int perPage);*/
+    /**
+     *app版本
+     */
+    @FormUrlEncoded
+    @POST("api/common/appVersion.do")
+    fun appVersion(@Field("key") key: String): Observable<BaseJson<Any>>
+}
---
+++ app/src/main/java/com/daomingedu/art/util/Utils.kt
@@ -0,0 +1,9 @@
+package com.daomingedu.art.util
+
+import android.app.Application
+
+class Utils {
+    companion object {
+        lateinit var app: Application
+    }
+}
